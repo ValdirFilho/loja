82e82a013c398ed25221006156959d5a7c176f82
Valdir Filho <valdir.silva60@etec.sp.gov.br>
2019-04-10 19:10:48 -0300

    upload do projeto

---
+++ Loja/src/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LABORATORIO_INFO
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.nome = "Palio";
+        carro.marca = "Fiat";
+        carro.cor = "Branco";
+        carro.fabricacao = "2016";
+        carro.largura = "1,63MM";
+        carro.peso = "940KG";
+
+        System.out.println(carro.nome);
+
+    }
+
+}
