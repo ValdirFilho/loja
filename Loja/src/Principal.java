d7121ddd8c4426046d36626ca140115830942ac5
brlucaslima <lucas.silva3100@etec.sp.gov.br>
2019-04-17 20:19:29 -0300

    First commit

--- Loja/src/Principal.java
+++ Loja/src/Principal.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,15 +15,34 @@ public class Principal {
 
     public static void main(String[] args) {
         Carro carro = new Carro();
-
-        carro.nome = "Palio";
-        carro.marca = "Fiat";
-        carro.cor = "Branco";
-        carro.fabricacao = "2016";
-        carro.largura = "1,63MM";
-        carro.peso = "940KG";
+        Scanner scan1=new Scanner(System.in);
+        System.out.print(carro.nome);
+        carro.nome=scan1.next();
+        System.out.print(carro.marca);
+        carro.marca=scan1.next();
+        System.out.print(carro.cor);
+        carro.cor=scan1.next();
+        System.out.print(carro.fabricacao);
+        carro.fabricacao=scan1.next();
+        System.out.print(carro.largura);
+        carro.largura=scan1.next();
+        System.out.print(carro.peso);
+        carro.peso=scan1.next();
+        
+        System.out.println("nome"+carro.nome);
+        System.out.println("marca"+carro.marca);
+        System.out.println("cor"+carro.cor);
+        System.out.println("fabricacao"+carro.fabricacao);
+        System.out.println("largura"+carro.largura);
+        System.out.println("peso"+carro.peso);
 
         System.out.println(carro.nome);
+        System.out.println(carro.marca);
+        System.out.println(carro.cor);
+        System.out.println(carro.fabricacao);
+        System.out.println(carro.largura);
+        System.out.println(carro.peso);
+        
 
     }
 
